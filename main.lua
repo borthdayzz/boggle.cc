57cdb10f3e12f0ffcfe11fa18db52ee63d062d60
BirthX <87570691+borthdayzz@users.noreply.github.com>
2024-11-11 11:27:23 +0800

    update

--- main.lua
+++ main.lua
@@ -1,1 +1,38 @@
-print("uhh")
+local function identifyExecutor()
+    if identifyexecutor then
+        return identifyexecutor()
+    else
+        return "Unknown"
+    end
+end
+-- nigga exploits
+    if identifyExecutor() == "Xeno" then
+        game:GetService("Players").LocalPlayer:Kick("Xeno will not work, fake ass unc why tf would you install this shit")
+    else
+    if identifyExecutor() == "FluxTeam" then
+        game:GetService("Players").LocalPlayer:Kick("FluxTeam will not work, fake ass unc why tf would you install this shit")
+    else
+    end
+end
+   -- destruction sim
+    if game.PlaceId == 2248408710 then
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/borthdayzz/boggle.cc/refs/heads/main/scripts/destructionsim.lua'),true))()
+    -- millionaire tycoon
+    elseif game.PlaceId == 6677985923 then
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/borthdayzz/boggle.cc/refs/heads/main/scripts/millionaire.lua'),true))()
+    -- heist tycoon
+    elseif game.PlaceId == 11103424163 then
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/borthdayzz/boggle.cc/refs/heads/main/scripts/heist.lua'),true))()
+    -- Diddy Plinko
+    elseif game.PlaceId == 18939066871 then
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/borthdayzz/boggle.cc/refs/heads/main/scripts/diddyplinko.lua'),true))()
+    -- Trench War
+    elseif game.PlaceId == 6654918151 then
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/borthdayzz/boggle.cc/refs/heads/main/scripts/trenchwar.lua'),true))()
+    -- Swat Simulator
+    elseif game.PlaceId == 2906554815 then
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/borthdayzz/boggle.cc/refs/heads/main/scripts/swat%20simulator.lua'),true))()
+    -- Universal
+    else
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/borthdayzz/boggle.cc/refs/heads/main/universal.lua'),true))()
+    end
